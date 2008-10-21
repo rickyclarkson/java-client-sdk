b873deb19e7e645cd6e4ab1dd52cd1f5071eef52
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2008-10-21 08:44:41 +0000

    Moving ArrayOrScalar into Variable
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@1665 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- protocol/src/main/java/uk/org/netvu/protocol/Variable.java
+++ protocol/src/main/java/uk/org/netvu/protocol/Variable.java
@@ -107,4 +107,20 @@ public enum Variable
     {
         return super.toString().toLowerCase() + ( arrayOrScalar == ArrayOrScalar.ARRAY ? "[]" : "" );
     }
+
+    /**
+     * Specifies whether a system variable is an array or a scalar value.
+     */
+    public enum ArrayOrScalar
+    {
+        /**
+         * Represents that the system variable is an array (0 or more values).
+         */
+        ARRAY,
+            
+        /**
+         * Represents that the system variable is a scalar (1 value).
+         */
+        SCALAR
+    }
 }
