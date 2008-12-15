b99d80ee976f836d4ec7697c8e3289791ed6fe3c
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2008-12-15 09:37:10 +0000

    Added tests for VideoFormat
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@2031 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

---
+++ data/src/test/scala/VideoFormatTest.scala
@@ -0,0 +1,18 @@
+package uk.org.netvu.data
+
+import _root_.org.{specs, scalacheck}
+import specs.runner.JUnit4
+import specs.Specification
+
+class VideoFormatTest extends JUnit4(new Specification {
+ "VideoFormat.valueOf" should {
+  "give a VideoFormat for values from 0 to 5 inclusive" in {
+   for (i <- 0 to 5)
+    VideoFormat.valueOf(i) == null mustBe false
+  }
+  "throw an IllegalArgumentException for values outside 0 to 5" in {
+   for (i <- List(-1, -100, 6, 100))
+    VideoFormat.valueOf(i) must throwA[IllegalArgumentException]
+  }
+ }
+})
