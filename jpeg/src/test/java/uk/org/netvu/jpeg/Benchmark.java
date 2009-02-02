97efd15fd096f5f88a4ac265515f46fa5ec33adc
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2009-02-02 14:26:50 +0000

    Implemented benchmarking against multiple images, and obtained some real data from cameras.
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@2323 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- jpeg/src/test/java/uk/org/netvu/jpeg/Benchmark.java
+++ jpeg/src/test/java/uk/org/netvu/jpeg/Benchmark.java
@@ -14,38 +14,50 @@ public class Benchmark
   {
     if (args.length == 0)
     {
-      for (int a = 0; a < 3; a ++)
+      for (int b = 0; b < 4; b ++)
       {
-        Process process = Runtime.getRuntime().exec(new String[]{"java", "-classpath", System.getProperty("java.class.path"), "uk.org.netvu.jpeg.Benchmark", String.valueOf(a)});
-        process.waitFor();
-        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
-        BufferedReader errorStreamReader = new BufferedReader(new InputStreamReader(process.getErrorStream(), "UTF-8"));
-        String line;
-        while ((line = inputStreamReader.readLine())!=null)
-          System.out.println(line);
-        while ((line = errorStreamReader.readLine())!=null)
-          System.err.println(line);
-        inputStreamReader.close();
-        errorStreamReader.close();
+        for (int a = 0; a < 3; a ++)
+        {
+          Process process = Runtime.getRuntime().exec(new String[]{"java", "-classpath", System.getProperty("java.class.path"), "uk.org.netvu.jpeg.Benchmark", String.valueOf(a), String.valueOf(b)});
+          process.waitFor();
+          BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
+          BufferedReader errorStreamReader = new BufferedReader(new InputStreamReader(process.getErrorStream(), "UTF-8"));
+          String line;
+          while ((line = inputStreamReader.readLine())!=null)
+            System.out.println(line);
+          while ((line = errorStreamReader.readLine())!=null)
+            System.err.println(line);
+          inputStreamReader.close();
+          errorStreamReader.close();
+        }
       }
     }
     else
     {
-      int which = Integer.parseInt(args[0]);
-      final ByteBuffer buffer = new FileInputStream("Jpegvergroessert.jpg").getChannel().map(FileChannel.MapMode.READ_ONLY, 0, new File("Jpegvergroessert.jpg").length());
+      int whichDecoder = Integer.parseInt(args[0]);
+      int whichImage = Integer.parseInt(args[1]);
+      
+      final String[] filenames = { "192-168-106-206-352x256.jpg", "mews-camvu-1-320x240.jpg", "mews-camvu-2-1600x1200.jpg", "Jpegvergroessert-129x256.jpg" };
+      
+      final ByteBuffer buffer = bufferFor(filenames[whichImage]);
     
-      Runnable[] decoders = { new Runnable() { public void run() { JPEGDecoders.adffmpegDecoder.decode(buffer); } },
-                              new Runnable() { public void run() { JPEGDecoders.toolkitDecoder.decode(buffer); } },
-                              new Runnable() { public void run() { JPEGDecoders.imageIODecoder.decode(buffer); } } };
+      Runnable[] decoders = { new Runnable() { public void run() { JPEGDecoders.adffmpegDecoder.decode(buffer); } public String toString() { return "adffmpeg"; } },
+                              new Runnable() { public void run() { JPEGDecoders.toolkitDecoder.decode(buffer); } public String toString() { return "toolkit"; } },
+                              new Runnable() { public void run() { JPEGDecoders.imageIODecoder.decode(buffer); } public String toString() { return "imageio"; } } };
                             
-      time(1000, 1000, decoders[which]);
+      time(1000, 1000, decoders[whichDecoder], filenames[whichImage]);
     }
   }
 
-  public static void time(int iterations, int warmUpMillis, Runnable runnable)
+  public static ByteBuffer bufferFor(String filename) throws IOException
+  {
+    return new FileInputStream(filename).getChannel().map(FileChannel.MapMode.READ_ONLY, 0, new File(filename).length());
+  }
+
+  public static void time(int iterations, long warmUpMillis, Runnable runnable, String extraInfo)
   {
     long start = System.nanoTime();
-    while (start + System.nanoTime() < start + warmUpMillis * 1000000)
+    while (System.nanoTime() - start > warmUpMillis * 1000000)
       runnable.run();
 
     start = System.nanoTime();
@@ -54,26 +66,6 @@ public class Benchmark
       runnable.run();
 
     long time = System.nanoTime() - start;
-    System.out.println(time + " for " + iterations + " iterations; " + time / iterations + " nanoseconds (" + time / iterations / 1000000 + " milliseconds ) per iteration.");
-  }
-
-  public static void main2(String[] args)
-  {
-    JFrame frame = new JFrame()
-      {{
-        add(new JPanel()
-          {{
-            try
-            {
-              add(new JLabel(new ImageIcon(JPEGDecoders.adffmpegDecoder.decode(new FileInputStream("Jpegvergroessert.jpg").getChannel().map(FileChannel.MapMode.READ_ONLY, 0, new File("Jpegvergroessert.jpg").length())))));
-            }
-            catch (IOException e)
-              {
-                throw new RuntimeException(e);
-              }
-          }});
-      }};
-    frame.setSize(400, 400);
-    frame.setVisible(true);
+    System.out.println(extraInfo + ": " + runnable + ": " + time + " for " + iterations + " iterations; " + time / iterations + " nanoseconds (" + time / iterations / 1000000 + " milliseconds ) per iteration or " + iterations * 1.0 / (time / 1000000000.0) + " iterations per second.");
   }
 }
