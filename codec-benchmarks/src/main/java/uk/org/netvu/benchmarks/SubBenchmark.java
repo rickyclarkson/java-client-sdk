2c0542a5f6ca18ec3bf17523c7058d32ba99b351
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2009-02-10 09:13:34 +0000

    Minor changes to get around an SVN issue.
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@2374 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

---
+++ codec-benchmarks/src/main/java/uk/org/netvu/benchmarks/SubBenchmark.java
@@ -0,0 +1,149 @@
+package uk.org.netvu.codecbenchmarks;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import uk.org.netvu.util.Function;
+import uk.org.netvu.util.Pair;
+import java.util.ArrayList;
+import java.util.List;
+import uk.org.netvu.adffmpeg.ADFFMPEGDecoder;
+import uk.org.netvu.jpeg.JPEGDecoder;
+import uk.org.netvu.jpeg.JPEGDecoders;
+import uk.org.netvu.jpeg.JPEGDecoderFromArray;
+import uk.org.netvu.jpeg.ToolkitDecoder;
+import uk.org.netvu.jpeg.ImageIODecoder;
+
+/**
+ * A program that is launched many times by Benchmark, to time the execution of
+ * a given number of iterations of a given decoder with a given test image.
+ */
+public class SubBenchmark
+{
+    /**
+     * The decoders to benchmark.
+     */
+  public static final List<Pair<JPEGDecoder, JPEGDecoderFromArray>> decoders = new ArrayList<Pair<JPEGDecoder, JPEGDecoderFromArray>>()
+  {
+    {
+      //this is an anonymous initialiser.
+      add(new Pair<JPEGDecoder, JPEGDecoderFromArray>(ADFFMPEGDecoder.getInstance(), ADFFMPEGDecoder.getInstance()));
+      add(new Pair<JPEGDecoder, JPEGDecoderFromArray>(ToolkitDecoder.getInstance(), ToolkitDecoder.getInstance()));
+      add(new Pair<JPEGDecoder, JPEGDecoderFromArray>(ImageIODecoder.getInstance(), ImageIODecoder.getInstance()));
+    }
+  };
+
+    public static <T> void humourousNetbeans(final int iterations, final Function<T, ?> decoder, final T input, long start, final String info) {
+        start = System.nanoTime();
+        for (int i = 0; i < iterations; i++) {
+            decoder.apply(input);
+        }
+        final long time = System.nanoTime() - start;
+        System.out.println(info + "," + iterations * 1.0 / (time / 1000000000.0));
+    }
+
+    /**
+     * Executes the specified decoder with the specified constraints.
+     * 
+     * @param args
+     *        the arguments to the process. The first argument is the index of
+     *        the decoder. The second argument is the index of the test data
+     *        file. The third argument is the index of the number of iterations
+     *        to perform. The fourth argument is the index of the number of
+     *        warmup times to perform. The fifth argument is the index of the
+     *        input type to use.
+     * @throws IOException
+     *         if any I/O error occurred.
+     */
+    public static void main( final String[] args ) throws IOException
+    {
+      final Pair<JPEGDecoder, JPEGDecoderFromArray> decoder = decoders.get(Integer.parseInt( args[0] ));
+        final SampleFile sampleFile = Benchmark.sampleFiles[Integer.parseInt( args[1] )];
+        final int iterations = Benchmark.iterationAmounts[Integer.parseInt( args[2] )];
+        final int warmUpTime = Benchmark.warmUpTimes[Integer.parseInt( args[3] )];
+        final String inputType = Benchmark.inputTypes[Integer.parseInt( args[4] )];
+
+        final String info =
+          decoder.getFirstComponent().getClass().getSimpleName() + "," + sampleFile.filename + "," + sampleFile.width + ","
+                        + sampleFile.height + "," + inputType + "," + warmUpTime + "," + iterations;
+
+        if ( inputType.equals( "ByteBuffer" ) )
+        {
+          time( iterations, warmUpTime, JPEGDecoders.decodeJPEG(decoder.getFirstComponent()), bufferFor( sampleFile.filename ), info );
+        }
+        else
+        {
+          time( iterations, warmUpTime, JPEGDecoders.decodeJPEGFromArray(decoder.getSecondComponent()), byteArrayFor( sampleFile.filename ), info );
+        }
+    }
+
+    /**
+     * Constructs a ByteBuffer containing the data from the specified file.
+     * 
+     * @param filename
+     *        the name of the file that contains the data to read.
+     * @return a ByteBuffer containing the data from the specified file.
+     * @throws IOException
+     *         if any I/O error occurs.
+     */
+    public static ByteBuffer bufferFor( final String filename ) throws IOException
+    {
+        final ByteBuffer first =
+                new FileInputStream( filename ).getChannel().map( FileChannel.MapMode.READ_ONLY, 0,
+                        new File( filename ).length() );
+        first.position( 0 );
+        final ByteBuffer result = ByteBuffer.allocateDirect( first.limit() );
+        result.put( first );
+        result.position( 0 );
+        return result;
+    }
+
+    /**
+     * Constructs an array of bytes containing the data from the specified file.
+     * 
+     * @param filename
+     *        the name of the file that contains the data to read.
+     * @return an array of bytes containing the data from the specified file.
+     * @throws IOException
+     *         if any I/O error occurs.
+     */
+    public static byte[] byteArrayFor( final String filename ) throws IOException
+    {
+        final ByteBuffer buffer = bufferFor( filename );
+        final byte[] bytes = new byte[buffer.limit()];
+        buffer.get( bytes );
+        return bytes;
+    }
+
+    /**
+     * Executes the specified decoder with the specified input for the specified
+     * number of iterations, after warming the decoder up by executing it for at
+     * least the specified number of milliseconds, printing the results to
+     * System.out as CSV.
+     * 
+     * @param iterations
+     *        the number of iterations to time.
+     * @param warmUpMillis
+     *        the number of milliseconds to give the decoder to warm up before
+     *        timing it.
+     * @param decoder
+     *        a Function that invokes the JPEGDecoder when applied.
+     * @param input
+     *        the input to the decoder.
+     * @param info
+     *        extra info to include in the output.
+     */
+    public static <T> void time( final int iterations, final long warmUpMillis, final Function<T, ?> decoder,
+            final T input, final String info )
+    {
+        long start = System.nanoTime();
+        while ( System.nanoTime() - start < warmUpMillis * 1000000 )
+        {
+            decoder.apply( input );
+        }
+        humourousNetbeans( iterations, decoder, input, start, info);
+    }
+}
