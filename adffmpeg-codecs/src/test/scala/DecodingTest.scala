eeea1e013009206259e4897f074d2ecfaf93aba8
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2009-02-11 12:31:48 +0000

    Attempting to improve coverage of adffmpeg-codecs
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@2403 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- adffmpeg-codecs/src/test/scala/DecodingTest.scala
+++ adffmpeg-codecs/src/test/scala/DecodingTest.scala
@@ -1,4 +1,4 @@
-package uk.org.netvu.jpeg
+package uk.org.netvu.adffmpeg
 
 import _root_.org.{specs, scalacheck}
 import specs.Specification
@@ -6,18 +6,19 @@ import specs.runner.JUnit4
 import java.nio.ByteBuffer
 import java.awt.Image
 import uk.org.netvu.util.Buffers
+import uk.org.netvu.jpeg.{JPEGDecoder, JPEGDecoderFromArray}
 
 class DecodingTest extends JUnit4(new Specification {
- for (decoder <- List[JPEGDecoder with JPEGDecoderFromArray](ToolkitDecoder.getInstance(), ImageIODecoder.getInstance()))//, ADFFMPEGDecoder.getInstance()))
+ for (decoder <- List[JPEGDecoder with JPEGDecoderFromArray](ADFFMPEGDecoder.getInstance()))
   decoder.toString + isSpecifiedBy(ValidDecoder(decoder))
 })
 
 case class ValidDecoder(decoder: JPEGDecoder with JPEGDecoderFromArray) extends Specification {
- for (file <- List((352, 256, "192-168-106-206-352x256.jpg" ),
-                   ( 320, 240, "mews-camvu-1-320x240.jpg" ),
-                   ( 1600, 1200, "mews-camvu-2-1600x1200.jpg" ),
-                   ( 320, 256, "192-168-106-207-320x256.jpg" ),
-                   ( 352, 256, "dvip3s-ad-dev-adh-352x256.jpeg" ))) {
+ for (file <- List((352, 256, "../jpeg/192-168-106-206-352x256.jpg" ),
+                   ( 320, 240, "../jpeg/mews-camvu-1-320x240.jpg" ),
+                   ( 1600, 1200, "../jpeg/mews-camvu-2-1600x1200.jpg" ),
+                   ( 320, 256, "../jpeg/192-168-106-207-320x256.jpg" ),
+                   ( 352, 256, "../jpeg/dvip3s-ad-dev-adh-352x256.jpeg" ))) {
                     ("Decoding " + file._3) should {
                      "give an Image whose width and height are the same as documented" in {
                       decoder.decodeJPEG(Buffers.bufferFor(file._3)) match { case image => image.getWidth(null) mustEqual file._1
