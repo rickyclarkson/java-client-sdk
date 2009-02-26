d826039c4620d2c6a33cfe7d2f79bfda2e55679f
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2009-02-26 16:36:00 +0000

    Removed an absolute path
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@2523 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- filter/src/test/scala/ManualCheck.scala
+++ filter/src/test/scala/ManualCheck.scala
@@ -8,8 +8,8 @@ import java.io._
 object Main { def main(args: Array[String]) = {
  val frame = new JFrame
  frame setLayout new FlowLayout
- frame add new JLabel(new ImageIcon("/home/ricky/java-client-sdk/trunk/jpeg/dvip3s-ad-dev-adh-352x256.jpeg"))
- frame add new JLabel(new ImageIcon(new MonochromeFilter().filter(ImageIO.read(new File("/home/ricky/java-client-sdk/trunk/jpeg/dvip3s-ad-dev-adh-352x256.jpeg")))))
+ frame add new JLabel(new ImageIcon("../jpeg/dvip3s-ad-dev-adh-352x256.jpeg"))
+ frame add new JLabel(new ImageIcon(new MonochromeFilter().filter(ImageIO.read(new File("../jpeg/dvip3s-ad-dev-adh-352x256.jpeg")))))
  frame.pack
  frame setVisible true
 } }
