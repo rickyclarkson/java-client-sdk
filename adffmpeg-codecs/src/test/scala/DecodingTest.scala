b3478ac43759d3fc4b1c2900c0078427fd129129
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2009-02-26 14:07:21 +0000

    Made adffmpeg-codecs have a test dependency on the test jar of jpeg Made jpeg have a test jar Made DecodingTest in both adffmpeg-codecs and jpeg use the same code without duplication. Implemented pixel-by-pixel comparison between the three codecs.
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@2517 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- adffmpeg-codecs/src/test/scala/DecodingTest.scala
+++ adffmpeg-codecs/src/test/scala/DecodingTest.scala
@@ -7,25 +7,9 @@ import java.nio.ByteBuffer
 import java.awt.Image
 import uk.org.netvu.util.Buffers
 import uk.org.netvu.jpeg.JPEGDecoder
+import uk.org.netvu.jpeg.DecodingTests
 
 class DecodingTest extends JUnit4(new Specification {
  for (decoder <- List[JPEGDecoder](ADFFMPEGDecoder.getInstance()))
-  decoder.toString + isSpecifiedBy(ValidDecoder(decoder))
+  decoder.toString + isSpecifiedBy(DecodingTests.validDecoder(decoder))
 })
-
-case class ValidDecoder(decoder: JPEGDecoder) extends Specification {
- for (file <- List(
-  (352, 256, "../jpeg/192-168-106-206-352x256.jpg" ),
-  ( 320, 240, "../jpeg/mews-camvu-1-320x240.jpg" ),
-  ( 1600, 1200, "../jpeg/mews-camvu-2-1600x1200.jpg" ),
-  ( 320, 256, "../jpeg/192-168-106-207-320x256.jpg" ),
-  ( 352, 256, "../jpeg/dvip3s-ad-dev-adh-352x256.jpeg" )
- )) {
-  ("Decoding " + file._3) should {
-   "give an Image whose width and height are the same as documented" in {
-    decoder.decodeJPEG(Buffers.bufferFor(file._3)) match { case image => image.getWidth(null) mustEqual file._1
-                                                           image.getHeight(null) mustEqual file._2 }
-   }
-  }
- }
-}
--- jpeg/src/test/scala/DecodingTest.scala
+++ jpeg/src/test/scala/DecodingTest.scala
@@ -8,7 +8,7 @@ import java.awt.Image
 import uk.org.netvu.util.Buffers
 import java.awt.image.BufferedImage
 
-object SampleData {
+object DecodingTests {
  val data = List((352, 256, "192-168-106-206-352x256.jpg"),
                  (320, 240, "mews-camvu-1-320x240.jpg"),
                  (1600, 1200, "mews-camvu-2-1600x1200.jpg"),
@@ -16,52 +16,52 @@ object SampleData {
                  (352, 256, "dvip3s-ad-dev-adh-352x256.jpeg")) map { case (_width, _height, _name) => new { val width = _width
                                                                                                             val height = _height
                                                                                                             val name = _name } }
-}
- 
-//parameterise the comparisons so that jpeg/ calls it with ImageIODecoder and adffmpeg-codecs with ADFFMPEGDecoder
-class DecodingTest extends JUnit4(new Specification {
- for (decoder <- List(ToolkitDecoder.createInstance(), ImageIODecoder.createInstance()))
-  decoder.toString + isSpecifiedBy(ValidDecoder(decoder))
 
- for (file <- SampleData.data) {
-  def decodeToIntArray(decoder: JPEGDecoder): Array[Int] = { val image = ToolkitDecoder.createInstance decodeJPEG Buffers.bufferFor(file.name)
-                                                             val bufferedImage = new BufferedImage(image getWidth null, image getHeight null, BufferedImage.TYPE_INT_ARGB)
-                                                             bufferedImage.getGraphics.drawImage(image, 0, 0, null)
-                                                             val dataBuffer = bufferedImage.getData.getDataBuffer
-                                                             val array = new Array[Int](dataBuffer.getSize)
-                                                             for (i <- 0 until array.length) array(i) = dataBuffer.getElem(i)
-                                                             array }
-
-  val control = decodeToIntArray(ToolkitDecoder.createInstance)
-  val fromImageIO = decodeToIntArray(ImageIODecoder.createInstance)
-
-  import Math.abs
-
-  def toRGB(i: Int) = (i >> 16 & 0xFF, i >> 8 & 0xFF, i & 0xFF)
-
-  def equal: Boolean = {
-   for { i <- 0 until control.length
-         (r, g, b) = toRGB(control(i))
-         (r2, g2, b2) = toRGB(fromImageIO(i))
-         if abs(r2 - r) > 5
-         if abs(g2 - g) > 5
-         if abs(b2 - b) > 5 } return false
-   return true
-  }
-  
-  "An image decoded by ImageIO" should { "be roughly equal to an image decoded by Toolkit" in {
-   equal must beTrue
-  } }
- }
-})
-
-case class ValidDecoder(decoder: JPEGDecoder) extends Specification {
- for (file <- SampleData.data) {
-  ("Decoding " + file.name) should {
-   "give an Image whose width and height are the same as documented" in {
-    decoder.decodeJPEG(Buffers.bufferFor(file.name)) match { case image => image.getWidth(null) mustEqual file.width
-                                                                           image.getHeight(null) mustEqual file.height }
+ def validDecoder(decoder: JPEGDecoder) = new Specification {
+  for (file <- data) {
+   ("Decoding " + file.name) should {
+    "give an Image whose width and height are the same as documented" in {
+     decoder.decodeJPEG(Buffers.bufferFor(file.name)) match { case image => image.getWidth(null) mustEqual file.width
+                                                             image.getHeight(null) mustEqual file.height }
+    }
    }
   }
+
+  for (file <- data) {
+   def decodeToIntArray(decoder: JPEGDecoder): Array[Int] = { val image = decoder decodeJPEG Buffers.bufferFor(file.name)
+                                                              val bufferedImage = new BufferedImage(image getWidth null, image getHeight null, BufferedImage.TYPE_INT_ARGB)
+                                                              bufferedImage.getGraphics.drawImage(image, 0, 0, null)
+                                                              val dataBuffer = bufferedImage.getData.getDataBuffer
+                                                              val array = new Array[Int](dataBuffer.getSize)
+                                                              for (i <- 0 until array.length) array(i) = dataBuffer.getElem(i)
+                                                              array }
+
+   val control = decodeToIntArray(ToolkitDecoder.createInstance)
+   val fromImageIO = decodeToIntArray(decoder)
+
+   import Math.abs
+
+   def toRGB(i: Int) = (i >> 16 & 0xFF, i >> 8 & 0xFF, i & 0xFF)
+
+   def equal: Boolean = {
+    for { i <- 0 until control.length
+          (r, g, b) = toRGB(control(i))
+          (r2, g2, b2) = toRGB(fromImageIO(i))
+          if abs(r2 - r) > 20
+          if abs(g2 - g) > 20
+          if abs(b2 - b) > 20 } return false
+    return true
+   }
+  
+   ("An image decoded by " + decoder) should { "be roughly equal to an image decoded by Toolkit" in {
+    equal must beTrue
+   } }
+  }
  }
 }
+ 
+class DecodingTest extends JUnit4(new Specification {
+ for (decoder <- List(ToolkitDecoder.createInstance(), ImageIODecoder.createInstance()))
+  decoder.toString + isSpecifiedBy(DecodingTests.validDecoder(decoder))
+})
+
