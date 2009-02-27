26e1f5e8809bd816d08a44612d650775b446730d
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2009-02-27 09:56:56 +0000

    Added tests for MPEG-4 decoding
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@2524 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- adffmpeg-codecs/src/test/scala/DecodingTest.scala
+++ adffmpeg-codecs/src/test/scala/DecodingTest.scala
@@ -8,8 +8,34 @@ import java.awt.Image
 import uk.org.netvu.util.Buffers
 import uk.org.netvu.jpeg.JPEGDecoder
 import uk.org.netvu.jpeg.DecodingTests
+import java.io.{File, FileInputStream, ByteArrayOutputStream}
 
-class DecodingTest extends JUnit4(new Specification {
+class JPEGDecodingTest extends JUnit4(new Specification {
  for (decoder <- List[JPEGDecoder](ADFFMPEGDecoder.getInstance()))
   decoder.toString + isSpecifiedBy(DecodingTests.validDecoder(decoder))
+})
+
+class MPEG4DecodingTest extends JUnit4(new Specification {
+ val decoder = ADFFMPEG4Decoder.getInstance
+
+ for {
+  a <- 0 until 5
+  file = new File("testdata/mpeg4frames/" + a)
+  buffer = { val in = new FileInputStream(file)
+             val out = new ByteArrayOutputStream
+             var b = in.read
+             while (b != -1) {
+              out write b
+              b = in.read
+             }
+             val bytes = out.toByteArray
+             val buffer = ByteBuffer allocateDirect bytes.length
+             buffer put bytes
+             buffer position 0
+             buffer }
+  image = decoder.decodeMPEG4(buffer)
+ } {
+  image.getWidth(null) > 10 mustBe true
+  image.getHeight(null) > 10 mustBe true
+ }
 })
