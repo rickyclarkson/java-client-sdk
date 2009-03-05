78f8db3b084704e5fd23102df9b8d64e8c3b6386
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2009-03-05 12:22:20 +0000

    Altered a test case to try to prevent an OutOfMemoryError
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@2592 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- adffmpeg-codecs/src/test/scala/DecodingTest.scala
+++ adffmpeg-codecs/src/test/scala/DecodingTest.scala
@@ -18,24 +18,23 @@ class JPEGDecodingTest extends JUnit4(new Specification {
 class MPEG4DecodingTest extends JUnit4(new Specification {
  val decoder = ADFFMPEGDecoders.getMPEG4Decoder
 
- for {
-  a <- 0 until 5
-  file = new File("testdata/mpeg4frames/" + a)
-  buffer = { val in = new FileInputStream(file)
-             val out = new ByteArrayOutputStream
-             var b = in.read
-             while (b != -1) {
-              out write b
-              b = in.read
-             }
-             val bytes = out.toByteArray
-             val buffer = ByteBuffer allocateDirect bytes.length
-             buffer put bytes
-             buffer position 0
-             buffer }
-  image = decoder.decodeMPEG4(buffer)
- } {
+ var a = 0
+ while (a < 5) {
+  val file = new File("testdata/mpeg4frames/" + a)
+  val in = new FileInputStream(file)
+  val out = new ByteArrayOutputStream
+  var b = in.read
+  while (b != -1) {
+   out write b
+   b = in.read
+  }
+  val bytes = out.toByteArray
+  val buffer = ByteBuffer allocateDirect bytes.length
+  buffer put bytes
+  buffer position 0
+  val image = decoder.decodeMPEG4(buffer)
   image.getWidth(null) > 10 mustBe true
   image.getHeight(null) > 10 mustBe true
+  a += 1
  }
 })
