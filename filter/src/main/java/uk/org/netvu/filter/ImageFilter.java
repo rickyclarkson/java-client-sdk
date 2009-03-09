94efae176d7bf89c4bbc2d2d5bef6ca5403e019f
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2009-03-09 12:24:43 +0000

    Reformatted source code
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@2639 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- filter/src/main/java/uk/org/netvu/filter/ImageFilter.java
+++ filter/src/main/java/uk/org/netvu/filter/ImageFilter.java
@@ -3,18 +3,19 @@ package uk.org.netvu.filter;
 import java.awt.Image;
 
 /**
- * A filter that takes in one {@link Image} and returns another after some processing.
- * The original <code>Image</code> is not changed.
+ * A filter that takes in one {@link Image} and returns another after some
+ * processing. The original <code>Image</code> is not changed.
  */
 public interface ImageFilter
 {
     /**
-     * Processes the specified <code>Image</code> to give an <code>Image</code> based on it.
+     * Processes the specified <code>Image</code> to give an <code>Image</code>
+     * based on it.
      * 
      * @param source
      *        the <code>Image</code> to process.
-     * @return an <code>Image</code> after some processing has been performed on the source
-     *         <code>Image</code>.
+     * @return an <code>Image</code> after some processing has been performed on
+     *         the source <code>Image</code>.
      */
     Image filter( Image source );
 }
