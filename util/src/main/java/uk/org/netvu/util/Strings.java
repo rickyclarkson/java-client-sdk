b20e9dffe536653f4578d196a250d3c5c2bb601d
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2010-03-04 13:51:44 +0000

    Making equalsIgnoreCase null-safe.
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@4278 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- util/src/main/java/uk/org/netvu/util/Strings.java
+++ util/src/main/java/uk/org/netvu/util/Strings.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 public class Strings {
     public static boolean equalsIgnoreCase(String one, String two) {
-        return one.toLowerCase(Locale.ENGLISH).equals(two.toLowerCase(Locale.ENGLISH));
+        return one == null ? two == null : two != null && one.toLowerCase(Locale.ENGLISH).equals(two.toLowerCase(Locale.ENGLISH));
     }
 
     public static int compareToIgnoreCase(String one, String two) {
