2db2778effc66f20e6f5bd2f2532e54c1a44d594
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2010-03-11 14:20:14 +0000

    Removed unused code.
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@4300 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- util/src/main/java/uk/org/netvu/util/Strings.java
+++ util/src/main/java/uk/org/netvu/util/Strings.java
@@ -6,8 +6,4 @@ public class Strings {
     public static boolean equalsIgnoreCase(String one, String two) {
         return one == null ? two == null : two != null && one.toLowerCase(Locale.ENGLISH).equals(two.toLowerCase(Locale.ENGLISH));
     }
-
-    public static int compareToIgnoreCase(String one, String two) {
-        return one.toLowerCase(Locale.ENGLISH).compareTo(two.toLowerCase(Locale.ENGLISH));
-    }
 }
