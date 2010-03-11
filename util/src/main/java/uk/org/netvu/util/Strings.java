d9d9f751a55dc870b6ea2cd80c557688ea11e8c9
rclarkson <rclarkson@4601b0d2-a6ba-4ff0-a3bb-59d57de6f651>
2010-03-11 14:30:11 +0000

    Reinstating code that was incorrectly thought to be unused.
    git-svn-id: https://192.168.106.29/svn/java-client-sdk/trunk@4306 4601b0d2-a6ba-4ff0-a3bb-59d57de6f651

--- util/src/main/java/uk/org/netvu/util/Strings.java
+++ util/src/main/java/uk/org/netvu/util/Strings.java
@@ -6,4 +6,8 @@ public class Strings {
     public static boolean equalsIgnoreCase(String one, String two) {
         return one == null ? two == null : two != null && one.toLowerCase(Locale.ENGLISH).equals(two.toLowerCase(Locale.ENGLISH));
     }
+
+    public static int compareToIgnoreCase(String one, String two) {
+        return one.toLowerCase(Locale.ENGLISH).compareTo(two.toLowerCase(Locale.ENGLISH));
+    }
 }
