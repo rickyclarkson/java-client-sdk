6bc9e0ea329413be6cbee82cce53254c05aa1175
Ricky Clarkson <ricky.clarkson@gmail.com>
2010-04-26 17:19:13 +0100

    Replaced Function with fj.F

--- util/src/main/java/uk/org/netvu/util/Lists.java
+++ util/src/main/java/uk/org/netvu/util/Lists.java
@@ -1,37 +1,33 @@
 package uk.org.netvu.util;
 
+import fj.F;
+
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 public final class Lists {
-    public static <T> List<T> filter(List<T> list, Function<T, Boolean> predicate) {
+    public static <T> List<T> filter(List<T> list, F<T, Boolean> predicate) {
         final List<T> results = new ArrayList<T>();
         for (T t: list)
-            if (predicate.apply(t)) 
+            if (predicate.f(t))
                 results.add(t);
         return results;
     }
 
-    public static <T, U> List<U> map(List<T> list, Function<T, U> function) {
+    public static <T, U> List<U> map(List<T> list, F<T, U> function) {
         final List<U> results = new ArrayList<U>();
         for (T t: list)
-            results.add(function.apply(t));
+            results.add(function.f(t));
         return results;
     }
 
-    public static <T> List<T> unique(List<T> list)
-    {
-        return new ArrayList<T>(new LinkedHashSet<T>(list));
-    }
-
-    public static <T> List<T> uniqueBy(List<T> list, Function<T, ? extends Object> by) {
+    public static <T> List<T> uniqueBy(List<T> list, F<T, ? extends Object> by) {
         final List<T> results = new ArrayList<T>();
         final Set<Object> set = new HashSet<Object>();
         for (T t: list) {
-            final Object b = by.apply(t);
+            final Object b = by.f(t);
             if (!set.contains(b))
                 results.add(t);
             set.add(b);
